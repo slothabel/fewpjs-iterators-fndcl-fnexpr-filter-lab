8a85abc216e5973c92dc83ac2dfbbff5573ae463
github username <yoshimejia00@gmail.comgit config --global user.name Yoshi>
2021-10-20 13:57:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+function findMatching(drivers, str){
+    let foundMatch = drivers.filter(driver => {
+        // without adding the return here, the iteration will not add the matching objects to the foundMatch array
+        return driver.toLowerCase() === str.toLowerCase()
+    })
+    return foundMatch
+}
+
+//  In this function, each element of the drivers array is a JavaScript object that has a property of name. 
+//The function should return each element whose name property matches the provided string argument.
+function fuzzyMatch(drivers, str){
+    let foundMatch = drivers.filter( driver =>
+        // lowercase current iterated driver. call indexOf on that lowercased version of the driver name
+        driver.toLowerCase().indexOf(str.toLowerCase()) 
+    )
+    return foundMatch
+}
+
+
+  
